c837b3e19dc94d704a5aded13d06eff796c14f80
vidboda <david.baux@inserm.fr>
2023-09-13 10:17:51 +0200

    adapted to internal cluster

--- autoAchabParall.sh
+++ autoAchabParall.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#! /bin/bash
 
 ###########################################################################
-#########							###########
-#########		AutoAchab				###########
-######### @uthor : D Baux	david.baux<at>inserm.fr		###########
-######### Date : 22/08/2018					###########
-#########							###########
+#########														###########
+#########		AutoAchabParall									###########
+######### @uthor : D Baux	david.baux<at>chu-montpellier.fr	###########
+######### Date : 22/08/2018										###########
+#########														###########
 ###########################################################################
 
 ###########################################################################
 ###########
 ########### 	Script to automate captainAchab WDL workflow
 ########### 	to annotate VCFs
-###########	see https://github.com/mobidic/MobiDL
+###########		see https://github.com/mobidic/MobiDL
 ###########
 ###########################################################################
 
@@ -26,17 +26,16 @@
 ####	or ensure you won't break your server with too many samples
 
 ##############		If any option is given, print help message	##################################
-VERSION=1.0
+VERSION=1.1
 USAGE="
-Program: AutoAchab
+Program: AutoAchabParall
 Version: ${VERSION}
-Contact: Baux David <david.baux@inserm.fr>
+Contact: Baux David <david.baux@chu-montpellier.fr>
 
 Usage: This script is meant to be croned
 	Should be executed once per minute
 
 "
-
 
 if [ $# -ne 0 ]; then
 	echo "${USAGE}"
@@ -52,7 +51,7 @@ warning() { log "[warn]" "$1" ; }
 info() { log "[info]" "$1" ; }
 debug() { log "[debug]" "$1" ; }
 
-# -- Print log 
+# -- Print log
 
 echoerr() { echo -e "$@" 1>&2 ; }
 
@@ -63,11 +62,10 @@ log() {
 
 
 ###############		Get options from conf file			##################################
+CONFIG_FILE='/mnt/Bioinfo/Softs/src/IURC/MobiDL_conf/autoAchabParall.conf'
+# CONFIG_FILE='./autoAchabParall.conf'
 
-CONFIG_FILE='./autoAchabParall.conf'
-
-#we check params against regexp
-
+# we check the params against a regexp
 UNKNOWN=$(cat  ${CONFIG_FILE} | grep -Evi "^(#.*|[A-Z0-9_]*=[a-z0-9_ \.\/\$\{\}]*)$")
 if [ -n "${UNKNOWN}" ]; then
 	error "Error in config file. Not allowed lines:"
@@ -90,12 +88,12 @@ fi
 
 ###############		functions for admin tasks
 
-admin() { 
+admin() {
 	#if sample_dir ne json sampleid
 	SAMPLE_OUT=$(awk -F"[ ,\"]" '/captainAchab.sampleID/{print $7}' "$4")
 	ADMIN_DIR="${DONE_DIR}/${SAMPLE_OUT}/$1"
 	mkdir "${ADMIN_DIR}"
-	"${RSYNC}" -az --exclude '*.vcf' "$2" "${ADMIN_DIR}" 
+	"${RSYNC}" -az --no-g --chmod=ugo=rwX --exclude '*.vcf' "$2" "${ADMIN_DIR}"
 	#cp "$2" "${ADMIN_DIR}"
 	#cp "$3" "${ADMIN_DIR}"
 	#cp "$4" "${ADMIN_DIR}"
@@ -104,23 +102,20 @@ admin() {
 }
 
 success() {
-	#OLDOLDOLDOLDsuccess exit code -"${DONE_DIR}/$1/CaptainAchab/admin" - "${TODO_DIR}/$1/captainAchab_inputs.json" - "${TODO_DIR}/$1/disease.txt" - ${LOG_FILE} - (Genuine|Relaunched) - ${SAMPLE}
-	#success exit code -"${DONE_DIR}/$1/CaptainAchab/admin" - (Genuine|Relaunched) - ${SAMPLE}
+	# success exit code - selfexpl - (Genuine|Relaunched) - ${SAMPLE} - selfexpl
 	if [ "$1" -eq 0 ];then
 		info "$3 Job finished for $4"
-		#admin "$2" "$3" "$4" "$5" "$7"
 		admin "$2" "${TODO_DIR}/${SAMPLE}/" "$4" "$5"
-		#admin "CaptainAchab/admin" "${TODO_DIR}/${SAMPLE}/captainAchab_inputs.json" "${TODO_DIR}/${SAMPLE}/disease.txt" "${LOG_FILE}" "${SAMPLE}"
-		exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1 
+		exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1
 		rm -rf "${TODO_DIR}/$4"
 	else
 		error "$4 was not treated correctly - Please contact an Admin to check log file at ${ERROR_DIR}/$4/autoAchab.log"
-		"${RSYNC}" -az "${TODO_DIR}/$4" "${ERROR_DIR}"
-		exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1 
+		"${RSYNC}" -az --no-g --chmod=ugo=rwX  "${TODO_DIR}/$4" "${ERROR_DIR}"
+		exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1
 		if [ "$?" -eq 0 ];then
 			rm -rf "${TODO_DIR}/$4"
 		fi
-		"${RSYNC}" -az "${DONE_DIR}/$4" "${ERROR_DIR}/$4"
+		"${RSYNC}" -az --no-g --chmod=ugo=rwX  "${DONE_DIR}/$4" "${ERROR_DIR}/$4"
 		chmod -R 777 "${ERROR_DIR}/$4"
 		rm -rf "${DONE_DIR}/$4"
 		exit 1
@@ -128,41 +123,39 @@ success() {
 }
 
 launch() {
-	#launch $SAMPLE - conf - $LOG_FILE - (Genuine|Relaunched)
+	# launch $SAMPLE - conf - $LOG_FILE - (Genuine|Relaunched)
 	if [ "${ACHABILARITY}" -eq 0 ];then
-		"${NOHUP}" "${SH}" "${CWW}" -e "${CROMWELL_JAR}" -o "${OPTIONS_JSON}" -c "$2" -w "${CAPTAINACHAB_WDL}" -i "${TODO_DIR}/$1/captainAchab_inputs.json" >>"$3" 2>&1
-		#success "$?" "CaptainAchab/admin" "${TODO_DIR}/$1/captainAchab_inputs.json" "${TODO_DIR}/$1/disease.txt" "$3" "$4" "$1"
-		#success exit code - selfexpl - selfexpl - selfexpl - ${LOG_FILE} - (Genuine|Relaunched) - ${SAMPLE}
+		# we activate the default gatkEnv here
+		source "${CONDABIN}activate" "${GATK_ENV}" 
+		"${NOHUP}" "${CWW}" -e "${CROMWELL_JAR}" -o "${OPTIONS_JSON}" -c "$2" -w "${CAPTAINACHAB_WDL}" -i "${TODO_DIR}/$1/captainAchab_inputs.json" >>"$3" 2>&1
 		success "$?" "CaptainAchab/admin" "$4" "$1" "${TODO_DIR}/$1/captainAchab_inputs.json"
-		#success exit code - selfexpl - (Genuine|Relaunched) - ${SAMPLE} - selfexpl
+		conda deactivate
+		# success exit code - selfexpl - (Genuine|Relaunched) - ${SAMPLE} - selfexpl
 	else
-		"${NOHUP}" "${SINGULARITY}" run -B "${ANNOVAR_PATH}:/media" -B "${DATA_MOUNT_POINT}:/mnt" ${ACHABILARITY_SIMG} -o "${OPTIONS_JSON}" -c "$2" -i "${TODO_DIR}/$1/captainAchab_inputs.json" >>"$3" 2>&1
+		"${SINGULARITY}" run -B "${ANNOVAR_PATH}:/media" -B "${DATA_MOUNT_POINT}:/mnt" ${ACHABILARITY_SIMG} -o "${OPTIONS_JSON}" -c "$2" -i "${TODO_DIR}/$1/captainAchab_inputs.json" >>"$3" 2>&1
 		success "$?" "CaptainAchab/admin" "$4" "$1" "${TODO_DIR}/$1/captainAchab_inputs.json"
 	fi
 }
 
 treat_sample() {
-	#${SAMPLE}
+	# ${SAMPLE}
 	##############		log file
 
 	LOG_FILE="${TODO_DIR}/${SAMPLE}/autoAchab.log"
 	touch ${LOG_FILE}
-	#exec &>${LOG_FILE}
 	exec 1>>${LOG_FILE} 2>&1
-	VCF=$(ls -l --time-style="long-iso" "${TODO_DIR}/${SAMPLE}" | egrep '^-' | awk '{print $8}' | egrep '*.vcf')	
-	if [ -f "${TODO_DIR}/${SAMPLE}/${VCF}" ] && [ -f "${TODO_DIR}/${SAMPLE}/captainAchab_inputs.json" ] && [ -f "${TODO_DIR}/${SAMPLE}/disease.txt" ];then 	
+	VCF=$(ls -l --time-style="long-iso" "${TODO_DIR}/${SAMPLE}" | egrep '^-' | awk '{print $8}' | egrep '*.vcf')
+	if [ -f "${TODO_DIR}/${SAMPLE}/${VCF}" ] && [ -f "${TODO_DIR}/${SAMPLE}/captainAchab_inputs.json" ] && [ -f "${TODO_DIR}/${SAMPLE}/disease.txt" ];then
 		echo ""
 		info "Launching captainAchab workflow for ${SAMPLE}, to follow check:"
 		info "tail -f ${LOG_FILE}"
-		launch "${SAMPLE}" "${CROMWELL_CONF_NODB_NOCACHE}" "${LOG_FILE}" "Genuine"
+		launch "${SAMPLE}" "${CROMWELL_CONF}" "${LOG_FILE}" "Genuine"
 	else
 		error "Folder incomplete or error in file names for sample ${SAMPLE}"
-		#exec 1 >> "${ERROR_DIR}/autoAchabError.log" 2>&1
-		#mv "${TODO_DIR}/${SAMPLE}" "${ERROR_DIR}"
-		${RSYNC} -az "${TODO_DIR}/${SAMPLE}" "${ERROR_DIR}"
+		${RSYNC} -az --no-g --chmod=ugo=rwX  "${TODO_DIR}/${SAMPLE}" "${ERROR_DIR}"
 		if [ "$?" -eq 0 ];then
-			exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1 
-			rm -r "${TODO_DIR}/${SAMPLE}"
+			exec 1>>"${ERROR_DIR}/autoAchabError.log" 2>&1
+			rm -rf "${TODO_DIR}/${SAMPLE}"
 		fi
 	fi
 }
@@ -170,29 +163,16 @@ treat_sample() {
 
 ###############         Now we'll have a look at the content of the directories #####################
 
-#http://moinne.com/blog/ronald/bash/list-directory-names-in-bash-shell
-#--time-style is used here to ensure awk $8 will return the right thing (dir name)
+# http://moinne.com/blog/ronald/bash/list-directory-names-in-bash-shell
+# --time-style is used here to ensure awk $8 will return the right thing (dir name)
 
-SAMPLES=$(ls -l --time-style="long-iso" ${TODO_DIR} | egrep '^d' | awk '{print $8}')
-#debug "Samples: --${SAMPLES}--"
+SAMPLES=$(ls -l --time-style="long-iso" ${TODO_DIR} | egrep '^d' | grep -v 'eaDir' | awk '{print $8}')
+# debug "Samples: --${SAMPLES}--"
 if [ "${SAMPLES}" != '' ];then
 	for SAMPLE in ${SAMPLES}
 	do
+		# debug "${SAMPLE}"
+		DONE_DIR=$(awk -F"[ ,\"]" '/captainAchab.outDir/{print $7}' "${TODO_DIR}/${SAMPLE}/captainAchab_inputs.json")
 		treat_sample "${SAMPLE}" &
-		##############		log file
-
-		#LOG_FILE="${TODO_DIR}/${SAMPLE}/autoAchab.log"
-		#touch ${LOG_FILE}
-		#exec &>${LOG_FILE}
-		#VCF=$(ls -l --time-style="long-iso" "${TODO_DIR}/${SAMPLE}" | egrep '^-' | awk '{print $8}' | egrep '*.vcf')	
-		#if [ -f "${TODO_DIR}/${SAMPLE}/${VCF}" ] && [ -f "${TODO_DIR}/${SAMPLE}/captainAchab_inputs.json" ] && [ -f "${TODO_DIR}/${SAMPLE}/disease.txt" ];then 	
-		#	echo ""
-		#	info "Launching captainAchab workflow for ${SAMPLE}, to follow check:"
-		#	info "tail -f ${LOG_FILE}"
-		#	launch "${SAMPLE}" "${CROMWELL_CONF}" "${LOG_FILE}" "Genuine"
-		#else
-		#	error "Folder incomplete or error in file names for sample ${SAMPLE}"
-		#	mv "${TODO_DIR}/${SAMPLE}" "${ERROR_DIR}"
-		#fi
 	done
 fi
